11326eed7603919f94a014c87615d67571f6de54
Sakin790 <sakinislam79@gmail.com>
2025-01-02 07:18:36 -0500

    db migration added

--- Project 4/src/index.ts
+++ Project 4/src/index.ts
@@ -1,41 +1,13 @@
-import 'dotenv/config';
-import { drizzle } from 'drizzle-orm/node-postgres';
-import { eq } from 'drizzle-orm';
-import { usersTable } from './db/schema';
-  
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { eq } from "drizzle-orm";
+import { usersTable } from "./db/schema";
+import express from "express";
+import { logger } from "./log/log";
 const db = drizzle(process.env.DATABASE_URL!);
 
-async function main() {
-  const user: typeof usersTable.$inferInsert = {
-    name: 'John',
-    age: 30,
-    email: 'john@example.com',
-  };
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-  await db.insert(usersTable).values(user);
-  console.log('New user created!')
-
-  const users = await db.select().from(usersTable);
-  console.log('Getting all users from the database: ', users)
-  /*
-  const users: {
-    id: number;
-    name: string;
-    age: number;
-    email: string;
-  }[]
-  */
-
-  await db
-    .update(usersTable)
-    .set({
-      age: 31,
-    })
-    .where(eq(usersTable.email, user.email));
-  console.log('User info updated!')
-
-  await db.delete(usersTable).where(eq(usersTable.email, user.email));
-  console.log('User deleted!')
-}
-
-main();
+app.listen(3000, () => logger.info("Server listening on localhost:3000"));
